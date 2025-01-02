e4afaa8da23663a93f67110c31723b7dd573e693
Karolina Glaza <kara.glaza@gmail.com>
2025-01-02 15:05:04 +0100

    New game option, working snake, new structs
    To do: snake killing itself

--- main.cpp
+++ main.cpp
@@ -14,8 +14,38 @@ extern "C" {
 #define GAME_HEIGHT 550
 
 #define CUBE_SIZE 10
+#define MAX_SNAKE_LENGTH 50
+#define HISTORY_SIZE (MAX_SNAKE_LENGTH*10)
 
-// Funkcja rysuj¹ca napis na powierzchni
+struct SDLStruct {
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Surface* screen;
+    SDL_Texture* scrtex;
+    SDL_Surface* charset;
+    SDL_Surface* body;
+};
+
+struct GameParameters {
+    bool quit;
+    double bodyX[MAX_SNAKE_LENGTH];
+    double bodyY[MAX_SNAKE_LENGTH];
+    double historyX[HISTORY_SIZE];
+    double historyY[HISTORY_SIZE];
+    int length;
+    double velocityX;
+    double velocityY;
+};
+
+struct GameTime {
+    int frames;
+    double fpsTimer;
+    double fps;
+    double worldTime;
+};
+
+
+// Funkcja rysujÄ…ca napis na powierzchni
 void DrawString(SDL_Surface* screen, int x, int y, const char* text, SDL_Surface* charset) {
     int px, py, c;
     SDL_Rect s, d;
@@ -37,7 +67,7 @@ void DrawString(SDL_Surface* screen, int x, int y, const char* text, SDL_Surface
     };
 }
 
-// Funkcja rysuj¹ca powierzchniê (sprite) na ekranie
+// Funkcja rysujÄ…ca powierzchniÄ™ (sprite) na ekranie
 void DrawSurface(SDL_Surface* screen, SDL_Surface* sprite, int x, int y) {
     SDL_Rect dest;
     dest.x = x - sprite->w / 2;
@@ -47,14 +77,14 @@ void DrawSurface(SDL_Surface* screen, SDL_Surface* sprite, int x, int y) {
     SDL_BlitSurface(sprite, NULL, screen, &dest);
 }
 
-// Funkcja rysuj¹ca pojedynczy piksel
+// Funkcja rysujÄ…ca pojedynczy piksel
 void DrawPixel(SDL_Surface* surface, int x, int y, Uint32 color) {
     int bpp = surface->format->BytesPerPixel;
     Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
     *(Uint32*)p = color;
 }
 
-// Funkcja rysuj¹ca liniê
+// Funkcja rysujÄ…ca liniÄ™
 void DrawLine(SDL_Surface* screen, int x, int y, int l, int dx, int dy, Uint32 color) {
     for (int i = 0; i < l; i++) {
         DrawPixel(screen, x, y, color);
@@ -63,7 +93,7 @@ void DrawLine(SDL_Surface* screen, int x, int y, int l, int dx, int dy, Uint32 c
     };
 }
 
-// Funkcja rysuj¹ca prostok¹t
+// Funkcja rysujÄ…ca prostokÄ…t
 void DrawRectangle(SDL_Surface* screen, int x, int y, int l, int k, Uint32 outlineColor, Uint32 fillColor) {
     int i;
     DrawLine(screen, x, y, k, 0, 1, outlineColor);
@@ -78,25 +108,13 @@ void DrawRectangle(SDL_Surface* screen, int x, int y, int l, int k, Uint32 outli
 extern "C"
 #endif
 
-
-int main(int argc, char** argv) {
-    bool quit;
-    bool new_game;
-    int t1, t2, frames, rc;
-    double delta, worldTime, fpsTimer, fps;
-    SDL_Event event;
-    SDL_Surface* screen, * charset;
-    SDL_Surface* body; // Zmienna do przechowywania powierzchni dla body.bmp
-    SDL_Texture* scrtex;
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-
+bool InitSDL(SDLStruct& sdl) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         printf("SDL_Init error: %s\n", SDL_GetError());
         return 1;
     }
 
-    rc = SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &window, &renderer);
+    int rc = SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, 0, &sdl.window, &sdl.renderer);
     if (rc != 0) {
         SDL_Quit();
         printf("SDL_CreateWindowAndRenderer error: %s\n", SDL_GetError());
@@ -104,171 +122,200 @@ int main(int argc, char** argv) {
     }
 
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderSetLogicalSize(sdl.renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+    SDL_SetRenderDrawColor(sdl.renderer, 0, 0, 0, 255);
 
-    SDL_SetWindowTitle(window, "Snake Movement Example");
+    SDL_SetWindowTitle(sdl.window, "Snake Movement Example");
 
-    screen = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
-    scrtex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+    sdl.screen = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+    sdl.scrtex = SDL_CreateTexture(sdl.renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     SDL_ShowCursor(SDL_DISABLE);
 
-    charset = SDL_LoadBMP("./cs8x8.bmp");
-    if (charset == NULL) {
+    sdl.charset = SDL_LoadBMP("./cs8x8.bmp");
+    if (sdl.charset == NULL) {
         printf("SDL_LoadBMP(cs8x8.bmp) error: %s\n", SDL_GetError());
-        SDL_FreeSurface(screen);
-        SDL_DestroyTexture(scrtex);
-        SDL_DestroyWindow(window);
-        SDL_DestroyRenderer(renderer);
+        SDL_FreeSurface(sdl.screen);
+        SDL_DestroyTexture(sdl.scrtex);
+        SDL_DestroyWindow(sdl.window);
+        SDL_DestroyRenderer(sdl.renderer);
         SDL_Quit();
         return 1;
     }
-    SDL_SetColorKey(charset, true, 0x000000);
+    SDL_SetColorKey(sdl.charset, true, 0x000000);
 
-    // Wczytanie obrazu body.bmp
-    body = SDL_LoadBMP("./body.bmp");
-    if (body == NULL) {
+    sdl.body = SDL_LoadBMP("./body.bmp");
+    if (sdl.body == NULL) {
         printf("SDL_LoadBMP(body.bmp) error: %s\n", SDL_GetError());
-        SDL_FreeSurface(charset);
-        SDL_FreeSurface(screen);
-        SDL_DestroyTexture(scrtex);
-        SDL_DestroyWindow(window);
-        SDL_DestroyRenderer(renderer);
+        SDL_FreeSurface(sdl.charset);
+        SDL_FreeSurface(sdl.screen);
+        SDL_DestroyTexture(sdl.scrtex);
+        SDL_DestroyWindow(sdl.window);
+        SDL_DestroyRenderer(sdl.renderer);
         SDL_Quit();
         return 1;
     }
-
-    char text[128];
-    int black = SDL_MapRGB(screen->format, 0x00, 0x00, 0x00);
-    int red = SDL_MapRGB(screen->format, 0xFF, 0x00, 0x00);
-    int blue = SDL_MapRGB(screen->format, 0x11, 0x11, 0xCC);
-
-    t1 = SDL_GetTicks();
-    frames = 0;
-    fpsTimer = 0;
-    fps = 0;
-    quit = 0;
-    worldTime = 0;
-    new_game = 0;
-
-    double bodyX[50] = { 0 };
-    double bodyY[50] = { 0 };
-    double historyX[500] = { 0 };
-    double historyY[500] = { 0 };
-
-    int historyLength = 500; // history complexity
-    int bodyLength = 10;     // length
-
-    bodyX[0] = SCREEN_WIDTH / 2; // head
-    bodyY[0] = SCREEN_HEIGHT / 2;
-
-    for (int i = 1; i < bodyLength; i++) {
-        bodyX[i] = bodyX[i - 1] + 20;
-        bodyY[i] = bodyY[i - 1];
-    }
-
-    // starting history 
-    for (int i = 0; i < historyLength; i++) {
-        historyX[i] = bodyX[0];
-        historyY[i] = bodyY[0];
-    }
-
-    double velocityX = 0;
-    double velocityY = 0;
-
-    while (!quit) {
-        t2 = SDL_GetTicks();
-        delta = (t2 - t1) * 0.001;
-        t1 = t2;
-
-        worldTime += delta;
-
-        SDL_FillRect(screen, NULL, black);
-
-        // history update(deleting useless elements)
-        for (int i = historyLength - 1; i > 0; i--) {
-            historyX[i] = historyX[i - 1];
-            historyY[i] = historyY[i - 1];
-        }
-        // writting head to history
-            historyX[0] = bodyX[0];
-            historyY[0] = bodyY[0];
-
-        // head movement
-        bodyX[0] += velocityX * delta;
-        bodyY[0] += velocityY * delta;
-
-        // going right when reaching boarders
-        if (bodyX[0] <= CUBE_SIZE && bodyY[0] <= CUBE_SIZE ) { velocityX = 200; velocityY = 0;}
-        else if (bodyX[0] >= SCREEN_WIDTH - CUBE_SIZE && bodyY[0] <= CUBE_SIZE) { velocityX = 0; velocityY = 200; }
-        else if (bodyX[0] >= SCREEN_WIDTH - CUBE_SIZE && bodyY[0] >= GAME_HEIGHT - CUBE_SIZE) { velocityX = -200; velocityY = 0; }
-        else if (bodyX[0] <= CUBE_SIZE && bodyY[0] >= GAME_HEIGHT - CUBE_SIZE) { velocityX = 0; velocityY = -200; }
-        else if (bodyX[0] <= CUBE_SIZE) { velocityX = 0; velocityY = -200; }
-        else if (bodyX[0] >= SCREEN_WIDTH - CUBE_SIZE) { velocityX = 0; velocityY = 200; }
-        else if (bodyY[0] <= CUBE_SIZE) { velocityX = 200; velocityY = 0; }
-        else if (bodyY[0] >= GAME_HEIGHT - CUBE_SIZE) { velocityX = -200; velocityY = 0; }
-
-        // going to the other side when reaching boarders
-        //if (bodyX[0] < 0) bodyX[0] = SCREEN_WIDTH;
-        //else if (bodyX[0] > SCREEN_WIDTH) bodyX[0] = 0;
-        //if (bodyY[0] < 0) bodyY[0] = GAME_HEIGHT;
-        //else if (bodyY[0] > GAME_HEIGHT) bodyY[0] = 0;
-
-        // other parts movement
-        for (int i = 1; i < bodyLength; i++) {
-            int historyIndex = i * (historyLength / bodyLength); // Dopasowanie odstêpu w historii
-            bodyX[i] = historyX[historyIndex];
-            bodyY[i] = historyY[historyIndex];
-        }
-
-        // drawing
-        for (int i = 0; i < bodyLength; i++) {
-            DrawSurface(screen, body, (int)bodyX[i], (int)bodyY[i]);
-        }
-
-        fpsTimer += delta;
-        if (fpsTimer > 0.5) {
-            fps = frames * 2;
-            frames = 0;
-            fpsTimer -= 0.5;
-        }
-
-        // info
-        DrawRectangle(screen, 4, GAME_HEIGHT + 4, SCREEN_WIDTH - 8, 36, red, blue);
-        sprintf(text, "Elapsed time = %.1lf s  %.0lf FPS", worldTime, fps);
-        DrawString(screen, screen->w / 2 - strlen(text) * 8 / 2, GAME_HEIGHT + 10, text, charset);
-        sprintf(text, "Esc - exit, Arrow keys - move");
-        DrawString(screen, screen->w / 2 - strlen(text) * 8 / 2, GAME_HEIGHT + 26, text, charset);
-
-        SDL_UpdateTexture(scrtex, NULL, screen->pixels, screen->pitch);
-        SDL_RenderCopy(renderer, scrtex, NULL, NULL);
-        SDL_RenderPresent(renderer);
-
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_ESCAPE) quit = 1;
-                else if(event.key.keysym.sym == 'n') new_game = 1;
-                else if (event.key.keysym.sym == SDLK_UP) { velocityX = 0; velocityY = -200; }
-                else if (event.key.keysym.sym == SDLK_DOWN) { velocityX = 0; velocityY = 200; }
-                else if (event.key.keysym.sym == SDLK_LEFT) { velocityX = -200; velocityY = 0; }
-                else if (event.key.keysym.sym == SDLK_RIGHT) { velocityX = 200; velocityY = 0; }
-                break;
-            case SDL_QUIT:
-                quit = 1;
-                break;
-            }
-        }
-        frames++;
-    }
-
-
-    SDL_FreeSurface(charset);
-    SDL_FreeSurface(screen);
-    SDL_DestroyTexture(scrtex);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
     return 0;
 }
+
+void CleanSDL(SDLStruct& sdl) {
+    SDL_FreeSurface(sdl.charset);
+    SDL_FreeSurface(sdl.screen);
+    SDL_FreeSurface(sdl.body);
+    SDL_DestroyTexture(sdl.scrtex);
+    SDL_DestroyRenderer(sdl.renderer);
+    SDL_DestroyWindow(sdl.window);
+    SDL_Quit();
+}
+
+void InitGame(GameParameters& game) {
+    game.quit = false;
+    game.length = 10; //beginer length 10 cubes
+    game.velocityX = 0;
+    game.velocityY = 0;
+
+    //starting positions 
+    game.bodyX[0] = SCREEN_WIDTH / 2;
+    game.bodyY[0] = SCREEN_HEIGHT / 2;
+    for (int i = 1; i < game.length; i++) {
+        game.bodyX[i] = game.bodyX[i - 1] + 20;
+        game.bodyY[i] = game.bodyY[i - 1];
+    }
+
+    //positions history
+    for (int i = 0; i < HISTORY_SIZE; i++) {
+        game.historyX[i] = game.bodyX[0];
+        game.historyY[i] = game.bodyY[0];
+    }
+}
+
+bool UserInput(GameParameters& game, SDL_Event& event) { //false - new game, true -default
+    if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_ESCAPE) game.quit = true;
+        else if (event.key.keysym.sym == 'n') return false;
+        else if (event.key.keysym.sym == SDLK_UP) { game.velocityX = 0; game.velocityY = -200; }
+        else if (event.key.keysym.sym == SDLK_DOWN) { game.velocityX = 0; game.velocityY = 200; }
+        else if (event.key.keysym.sym == SDLK_LEFT) { game.velocityX = -200; game.velocityY = 0; }
+        else if (event.key.keysym.sym == SDLK_RIGHT) { game.velocityX = 200; game.velocityY = 0; }
+    }
+    return true;
+}
+
+void MoveSnake(GameParameters& game, GameTime& time, double delta) {
+    // history update(deleting useless elements)
+    for (int i = HISTORY_SIZE - 1; i > 0; i--) {
+        game.historyX[i] = game.historyX[i - 1];
+        game.historyY[i] = game.historyY[i - 1];
+    }
+    // writting head to history
+    game.historyX[0] = game.bodyX[0];
+    game.historyY[0] = game.bodyY[0];
+
+    // head movement
+    game.bodyX[0] += game.velocityX * delta;
+    game.bodyY[0] += game.velocityY * delta;
+
+    // going right when reaching boarders
+    if (game.bodyX[0] <= CUBE_SIZE && game.bodyY[0] <= CUBE_SIZE) { game.velocityX = 200; game.velocityY = 0; }
+    else if (game.bodyX[0] >= SCREEN_WIDTH - CUBE_SIZE && game.bodyY[0] <= CUBE_SIZE) { game.velocityX = 0; game.velocityY = 200; }
+    else if (game.bodyX[0] >= SCREEN_WIDTH - CUBE_SIZE && game.bodyY[0] >= GAME_HEIGHT - CUBE_SIZE) { game.velocityX = -200; game.velocityY = 0; }
+    else if (game.bodyX[0] <= CUBE_SIZE && game.bodyY[0] >= GAME_HEIGHT - CUBE_SIZE) { game.velocityX = 0; game.velocityY = -200; }
+    else if (game.bodyX[0] <= CUBE_SIZE) { game.velocityX = 0; game.velocityY = -200; }
+    else if (game.bodyX[0] >= SCREEN_WIDTH - CUBE_SIZE) { game.velocityX = 0; game.velocityY = 200; }
+    else if (game.bodyY[0] <= CUBE_SIZE) { game.velocityX = 200; game.velocityY = 0; }
+    else if (game.bodyY[0] >= GAME_HEIGHT - CUBE_SIZE) { game.velocityX = -200; game.velocityY = 0; }
+
+    // going to the other side when reaching boarders
+    //if (bodyX[0] < 0) bodyX[0] = SCREEN_WIDTH;
+    //else if (bodyX[0] > SCREEN_WIDTH) bodyX[0] = 0;
+    //if (bodyY[0] < 0) bodyY[0] = GAME_HEIGHT;
+    //else if (bodyY[0] > GAME_HEIGHT) bodyY[0] = 0;
+
+    // other parts movement
+    for (int i = 1; i < game.length; i++) {
+        int historyIndex = i * (HISTORY_SIZE / game.length) /2; // divided - to have smooth snake
+        game.bodyX[i] = game.historyX[historyIndex];
+        game.bodyY[i] = game.historyY[historyIndex];
+    }
+}
+
+void Draw(SDLStruct& sdl, GameParameters& game, GameTime& time) {
+    char text[128];
+
+    SDL_FillRect(sdl.screen, NULL, SDL_MapRGB(sdl.screen->format, 0x00, 0x00, 0x00));
+
+    // Snake
+    for (int i = 0; i < game.length; i++) {
+        DrawSurface(sdl.screen, sdl.body, (int)game.bodyX[i], (int)game.bodyY[i]);
+    }
+
+    // Updates 
+    DrawRectangle(sdl.screen, 4, GAME_HEIGHT + 4, SCREEN_WIDTH - 8, 36, SDL_MapRGB(sdl.screen->format, 0xFF, 0x00, 0x00), SDL_MapRGB(sdl.screen->format, 0x11, 0x11, 0xCC));
+    sprintf(text, "Elapsed time = %.1lf s  %.0lf FPS", time.worldTime, time.fps);
+    DrawString(sdl.screen, sdl.screen->w / 2 - strlen(text) * 8 / 2, GAME_HEIGHT + 10, text, sdl.charset);
+    sprintf(text, "Esc - exit, Arrow keys - move");
+    DrawString(sdl.screen, sdl.screen->w / 2 - strlen(text) * 8 / 2, GAME_HEIGHT + 26, text, sdl.charset);
+
+    SDL_UpdateTexture(sdl.scrtex, NULL, sdl.screen->pixels, sdl.screen->pitch);
+    SDL_RenderCopy(sdl.renderer, sdl.scrtex, NULL, NULL);
+    SDL_RenderPresent(sdl.renderer);
+}
+
+
+int main(int argc, char** argv) {
+    SDLStruct sdl;
+    GameParameters game;
+    GameTime time = { 0, 0, 0, 0 };
+
+    if (InitSDL(sdl)) return 1;
+    InitGame(game);
+
+    int t1 = SDL_GetTicks();
+
+    while (!game.quit) {
+        int t2 = SDL_GetTicks();
+        double delta = (t2 - t1) * 0.001;
+        t1 = t2;
+
+        time.worldTime += delta;
+        time.fpsTimer += delta;
+        time.frames++;
+
+        if (time.fpsTimer > 0.5) {
+            time.fps = time.frames * 2;
+            time.frames = 0;
+            time.fpsTimer -= 0.5;
+        }
+
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (!UserInput(game, event)) {
+                CleanSDL(sdl);
+                time = { 0, 0, 0, 0 };
+                if (InitSDL(sdl)) return 1;
+                InitGame(game);
+                int t1 = SDL_GetTicks();
+                double delta = (t2 - t1) * 0.001;
+                t1 = t2;
+
+                time.worldTime += delta;
+                time.fpsTimer += delta;
+                time.frames++;
+
+                if (time.fpsTimer > 0.5) {
+                    time.fps = time.frames * 2;
+                    time.frames = 0;
+                    time.fpsTimer -= 0.5;
+                }
+            }
+        }
+
+        MoveSnake(game, time, delta);
+        Draw(sdl, game, time);
+    }
+
+    CleanSDL(sdl);
+    return 0;
+}
+
+
